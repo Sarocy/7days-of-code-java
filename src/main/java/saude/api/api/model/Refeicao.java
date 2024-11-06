6808c3906138ff268de3f7899491b28ee9094fac
Sarocy <sarahocy@hotmail.com>
2024-11-06 16:28:40 -0300

    Criação da model

---
+++ src/main/java/saude/api/api/model/Refeicao.java
@@ -0,0 +1,23 @@
+package saude.api.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+public class Refeicao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String tipo;
+    private Double quantidade;
+    private LocalDate data;
+}
